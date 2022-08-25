14e0c922046410fedc2d556964d0e7e8e5e64194
Package <ryan.dean1000@outlook.com>
2022-08-25 02:24:38 +0100

    Add search params to API call to filter vacancies.

--- services/vacancy-service.ts
+++ services/vacancy-service.ts
@@ -1,7 +1,8 @@
 import { ApiVacancyResponse, SearchState, Vacancy } from "../types";
+import { convertPostCodeToLatLng } from "./postcode-service";
 
 export async function getAllVacancies(searchState: SearchState): Promise<Vacancy[]> {
-    const url = `${process.env.API_URL}/vacancy?PageSize=100`;
+    const url = await buildRequestUrl(searchState);
     const response = await fetch(url, {
         headers: {
             'Ocp-Apim-Subscription-Key': process.env.API_KEY as string,
@@ -23,4 +24,24 @@ export async function getVacancyByReference(reference: string): Promise<Vacancy>
     });
 
     return await response.json() as Vacancy;
+}
+
+async function buildRequestUrl(searchState: SearchState): Promise<string> {
+    let url = `${process.env.API_URL}/vacancy?PageSize=100`;
+
+    // Apply filter by location
+    if (searchState.location && searchState.miles) {
+        const latLngPair = await convertPostCodeToLatLng(searchState.location);
+
+        url += `&Lat=${latLngPair.lat}&Lon=${latLngPair.lon}`;
+        url += `&DistanceInMiles=${searchState.miles}`;
+        url += `&Sort=DistanceAsc`
+    }
+
+    // Apply filter by specific learning aim
+    if (searchState.larsCode) {
+        url += `&StandardLarsCode=${searchState.larsCode}`;
+    }
+
+    return url;
 }
